72bc8475e3ed0c8756d5fdc8d2df153975fb42e9
Matthew Johnston <mattdjohnston92@gmail.com>
2021-09-11 09:37:20 -0400

    Add updated my_fixer.js file.

--- lesson_1/dom_shuffling_completed/my_fixer.js
+++ lesson_1/dom_shuffling_completed/my_fixer.js
@@ -1,25 +1,15 @@
+"use strict";
 
 let h1 = document.querySelector('h1');
-h1.remove();
-
 let headerNav = document.body.firstElementChild.nextElementSibling;
-headerNav.remove();
 headerNav.insertAdjacentElement('afterbegin', h1);
-
 document.body.insertAdjacentElement('afterbegin', headerNav);
 
 let [babyMop, chinStick] = document.querySelectorAll('img');
-babyMop.remove();
-chinStick.remove();
-
 let [chinStickFigure, babyMopFigure] = document.querySelectorAll('figure');
-chinStickFigure.remove();
-babyMopFigure.remove();
-
 chinStickFigure.insertAdjacentElement('afterbegin', chinStick);
 babyMopFigure.insertAdjacentElement('afterbegin', babyMop);
 
 let article = document.querySelector('#content article');
 article.insertAdjacentElement('beforeend', chinStickFigure);
 article.insertAdjacentElement('beforeend', babyMopFigure);
-
