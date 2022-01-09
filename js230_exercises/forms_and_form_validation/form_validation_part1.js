d9a5c2f1e58b819d5c030c41dd7639a9d781f3ad
Matthew Johnston <mattdjohnston92@gmail.com>
2022-01-09 12:53:05 -0500

    Add js230 exercise files for form validation part 1 problem of forms and form validation problem set.

---
+++ js230_exercises/forms_and_form_validation/form_validation_part1.js
@@ -0,0 +1,200 @@
+"use strict";
+
+let App = {
+  isFirstNameValid: function() {
+    return !this.firstNameInput.validity.valueMissing;
+  },
+
+  isLastNameValid: function() {
+    return !this.lastNameInput.validity.valueMissing;
+  },
+
+  isEmailValid: function() {
+    return !this.emailInput.validity.valueMissing && !this.emailInput.validity.patternMismatch;
+  },
+
+  isPasswordValid: function() {
+    return !this.passwordInput.validity.valueMissing && !this.passwordInput.validity.tooShort;
+  },
+
+  isPhoneNumberValid: function() {
+    return !this.phoneInput.validity.patternMismatch;
+  },
+
+  displayFirstNameErrorMessage: function() {
+    let firstNameErrorElement = this.firstNameInput.nextElementSibling;
+    firstNameErrorElement.innerText = 'First name is a required field.';
+    this.firstNameInput.classList.add('invalid');
+  },
+
+  displayLastNameErrorMessage: function() {
+    let lastNameErrorElement = this.lastNameInput.nextElementSibling;
+    lastNameErrorElement.innerText = 'Last name is a required field.';
+    this.lastNameInput.classList.add('invalid');
+  },
+
+  displayEmailErrorMessage: function() {
+    this.emailInput.classList.add('invalid');
+    let emailErrorElement = this.emailInput.nextElementSibling;
+
+    if (this.emailInput.validity.valueMissing) {
+      emailErrorElement.innerText = 'Email is a required field.';
+    } else {
+      emailErrorElement.innerText = 'Please enter a valid email.';
+    }    
+  },
+
+  displayPasswordErrorMessage: function() {
+    this.passwordInput.classList.add('invalid');
+    let passwordErrorElement = this.passwordInput.nextElementSibling;
+
+    if (this.passwordInput.validity.valueMissing) {
+      passwordErrorElement.innerText = 'Password is a required field.';
+    } else {
+      passwordErrorElement.innerText = 'Password must be at least 10 characters long.';
+    }    
+  },
+
+  displayPhoneNumberErrorMessage: function() {
+    let phoneErrorElement = this.phoneInput.nextElementSibling;
+    phoneErrorElement.innerText = 'Please enter a valid phone number.';
+    this.phoneInput.classList.add('invalid');
+  },
+
+  removeFirstNameErrorMessage: function() {
+    let firstNameErrorElement = this.firstNameInput.nextElementSibling;
+    firstNameErrorElement.innerText = '';
+    this.firstNameInput.classList.remove('invalid');
+  },
+
+  removeLastNameErrorMessage: function() {
+    let lastNameErrorElement = this.lastNameInput.nextElementSibling;
+    lastNameErrorElement.innerText = '';
+    this.lastNameInput.classList.remove('invalid');
+  },
+
+  removeEmailErrorMessage: function() {
+    let emailErrorElement = this.emailInput.nextElementSibling;
+    emailErrorElement.innerText = '';
+    this.emailInput.classList.remove('invalid');
+  },
+
+  removePasswordErrorMessage: function() {
+    let passwordErrorElement = this.passwordInput.nextElementSibling;
+    passwordErrorElement.innerText = '';
+    this.passwordInput.classList.remove('invalid');
+  },
+
+  removePhoneNumberErrorMessage: function() {
+    let phoneErrorElement = this.phoneInput.nextElementSibling;
+    phoneErrorElement.innerText = '';
+    this.phoneInput.classList.remove('invalid');
+  },
+
+  validateFirstNameInput: function() {
+    this.firstNameInput.addEventListener('blur', () => {
+      if (!this.isFirstNameValid()) {
+        this.displayFirstNameErrorMessage();
+      }
+    });
+
+    this.firstNameInput.addEventListener('focus', () => {
+      this.removeFirstNameErrorMessage();
+    });
+  },
+
+  validateLastNameInput: function() {
+    this.lastNameInput.addEventListener('blur', () => {
+      if (!this.isLastNameValid()) {
+        this.displayLastNameErrorMessage();
+      }
+    });
+
+    this.lastNameInput.addEventListener('focus', () => {
+      this.removeLastNameErrorMessage();
+    });
+  },
+
+  validateEmailInput: function() {
+    this.emailInput.addEventListener('blur', () => {
+      if (!this.isEmailValid()) {
+        this.displayEmailErrorMessage();
+      }
+    });
+
+    this.emailInput.addEventListener('focus', () => {
+      this.removeEmailErrorMessage();
+    });
+  },
+
+  validatePasswordInput: function() {
+    this.passwordInput.addEventListener('blur', () => {
+      if (!this.isPasswordValid()) {
+        this.displayPasswordErrorMessage();
+      }
+    });
+
+    this.passwordInput.addEventListener('focus', () => {
+      this.removePasswordErrorMessage();
+    });
+  },
+
+  validatePhoneNumberInput: function() {
+    this.phoneInput.addEventListener('blur', () => {
+      if (!this.isPhoneNumberValid()) {
+        this.displayPhoneNumberErrorMessage();
+      }
+    });
+
+    this.phoneInput.addEventListener('focus', () => {
+      this.removePhoneNumberErrorMessage();
+    });
+  },
+
+  validateAllOnSubmission() {
+    document.querySelector('button').addEventListener('click', event => {
+      event.preventDefault();
+
+      if (!this.isFirstNameValid()) {
+        this.displayFirstNameErrorMessage();
+        console.log('value missing');
+      }
+
+      if (!this.isLastNameValid()) {
+        this.displayLastNameErrorMessage();
+      }
+
+      if (!this.isEmailValid()) {
+        this.displayEmailErrorMessage();
+      }
+
+      if (!this.isPasswordValid()) {
+        this.displayPasswordErrorMessage();
+      }
+
+      if (!this.isPhoneNumberValid()) {
+        this.displayPhoneNumberErrorMessage();
+      }
+    });
+  },
+  
+  init: function() {
+    this.firstNameInput = document.getElementById('first-name');
+    this.lastNameInput = document.getElementById('last-name');
+    this.emailInput = document.getElementById('email');
+    this.passwordInput = document.getElementById('password');
+    this.phoneInput = document.getElementById('phone');
+    return this;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let app = App.init();
+
+  app.validateFirstNameInput();
+  app.validateLastNameInput();
+  app.validateEmailInput();
+  app.validatePasswordInput();
+  app.validatePhoneNumberInput();
+  app.validateAllOnSubmission();
+});
